7bfdd55c173ee132dea41d4aa508c29b84feef46
Barry Anil <39921790+Barry-Anil@users.noreply.github.com>
2023-07-20 22:09:44 +0530

    Update store-config.js

--- store-config.js
+++ store-config.js
@@ -1,10 +1,10 @@
 // CORS when consuming Medusa from admin
 const ADMIN_CORS = process.env.ADMIN_CORS || 
-  "http://localhost:7000,http://localhost:7001, http://localhost:9000, https://medusabackend-production-c032.up.railway.app:9000"
+  "http://localhost:7000,http://localhost:7001, http://localhost:9000, https://medusabackend-production-c032.up.railway.app"
 
 // CORS to avoid issues when consuming Medusa from a client
 const STORE_CORS = 
-  process.env.STORE_CORS || "https://medusabackend-production-c032.up.railway.app:9000" 
+  process.env.STORE_CORS || "https://medusa-storefront-teal.vercel.app/" 
 
   function withStoreConfig(nextConfig = {}) {
     const features = nextConfig.features || {}
